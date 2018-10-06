908b0059350ff438d666cc3da79f1a9a4688bd5c
Andrija <aperusic18@gmail.com>
2018-10-06 23:37:50 +0200

    Add comments for handleClick function

--- src/App.js
+++ src/App.js
@@ -17,12 +17,15 @@ class App extends Component {
     };
   }
 
+  //central function that handles logic for calculator behaviour
   handleClick(button) {
 
     if(isNumeric(button)) {
 
+      //16 is the maximum number of digits
       if(this.state.value !== null && this.state.value.length >= 16) return;
 
+      //initial zero clicks dont't make any difference
       if(button === "0" && (this.state.value === null || this.state.value === "0")) return this.setState({value: "0"});
 
       if(button === "." && this.state.value === null) return this.setState({value: "0."});
@@ -34,12 +37,18 @@ class App extends Component {
       } 
 
     } else if (isOperation(button)) {
+
+        //if operation is not given yet
         if(this.state.operation === null) {
+          //and there is no value on screen, just store the operation 
           if(!this.state.value) {
             this.setState({operation: button});
+          //and there is value, then store operation and store value into cachedOperand
           } else {
             this.setState({value: null, operation: button, cachedOperand: this.state.value});
-          }      
+          }
+        
+        //if operation already exists, then fetch the result, store it into cached operand and store new operation
         } else {
           let currentOperand = this.state.value || 0;
           this.fetchResult(this.state.cachedOperand, currentOperand, this.state.operation)
@@ -49,12 +58,15 @@ class App extends Component {
             .catch(e => { console.log("Unable to fetch result from calculator API") });
         }
 
+    //all clear
     } else if (button === "AC") {
       this.setState({value: null, operation: null, cachedOperand: null});
 
+    //clear - clears only display
     } else if (button === "C") {
       this.setState({value: null});
 
+    //fetch result when pressing equals only when there exists stored operation and cachedOperand and store result into cachedOperand
     } else if (button === "=") {
       if(this.state.operation && this.state.cachedOperand) {
         let currentOperand = this.state.value || 0;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,7 @@ const isNumeric = n => (!isNaN(parseFloat(n)) && isFinite(n)) || n === ".";
 
 const isOperation = n => n === "/" || n === "*" || n === "-" || n === "+";
 
+//if result of operation is longer than 16 digits this function tuns it into 0
 const trimResult = (result) => {
     result = result.toString();
     if(result.length > 16) return "0";
